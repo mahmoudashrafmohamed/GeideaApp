7533acc1c734798789abd55ec458e7fe3e150104
mahmoud_ashraf <mahmoud.ashraf@orcas.io>
2022-07-19 13:22:49 +0200

    add hilt for DI

--- app/src/main/java/com/mahmoudashraf/geideaapp/presentation/MainActivity.kt
+++ app/src/main/java/com/mahmoudashraf/geideaapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mahmoudashraf.geideaapp
+package com.mahmoudashraf.geideaapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mahmoudashraf.geideaapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
